5f59e5f661ea970027f3d9eab9e833c5d957d34b
vincenzoarena <penninoematita@gmail.com>
2023-01-13 12:30:20 +0100

    Setting up new project

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Paradise Island</a>
+      </header>
+      <main>
+        Listed Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
